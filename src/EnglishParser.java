ebbb0fdd8eb3b649e27a5e0880efb8e61563c6f9
Alexander <81604280@qq.com>
2022-06-06 15:31:54 +0800

    开发EnglishParser类

--- src/EnglishParser.java
+++ src/EnglishParser.java
@@ -1,12 +1,17 @@
 import lib.Parser;
+import lib.PrintHelper;
 import lib.Token;
 import lib.ast.ASTNode;
+import lib.ast.InnerNode;
 
 import java.util.List;
 
 public class EnglishParser {
   public ASTNode parse(List<Token> tokens) {
-    return null;
+    var englishParser = new Parser("<sentence>");
+    englishParser.terminal("I").terminal("VERB").terminal("NOUN");
+    var root = englishParser.parse(tokens);
+    return root;
   }
 
   public ASTNode parseMatchingParenthesis(List<Token> tokens) {
