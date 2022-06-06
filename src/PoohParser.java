4fefec818b88c40a0da5565db66f9a93b14348b4
Alexander_Lu <81604280@qq.com>
2022-06-06 19:06:15 +0800

    请你修改PoohParser类，使得PoohParser.parse可以解析上述语法。

--- src/PoohParser.java
+++ src/PoohParser.java
@@ -1,3 +1,4 @@
+import lib.Parser;
 import lib.Token;
 import lib.ast.ASTNode;
 
@@ -11,6 +12,19 @@ public class PoohParser {
    * @return 解析出的抽象语法树AST
    */
   public ASTNode parse(List<Token> tokens) {
-    return null;
+    var program = new Parser("<program>");
+    var statement = new Parser("<statement>");
+    var printStatement = new Parser("<print-statement>");
+    var assignStatement = new Parser("<assign-statement>");
+    var ifStatement = new Parser("<if-statement>");
+    var block = new Parser("<block>");
+    program.nonTerminal(statement).nonTerminal(program).orEpsilon();
+    statement.nonTerminal(printStatement).or().nonTerminal(assignStatement).or().nonTerminal(ifStatement);
+    printStatement.terminal("KEYWORD_PRINT").terminal("ID");
+    assignStatement.terminal("ID").terminal("EQUAL").terminal("NUMBER");
+    ifStatement.terminal("KEYWORD_IF").terminal("LEFT_PAR").terminal("ID").terminal("LESS_THAN").terminal("ID").terminal("RIGHT_PAR").nonTerminal(block).terminal("KEYWORD_ELSE").nonTerminal(block);
+    block.terminal("LEFT_BRACE").nonTerminal(program).terminal("RIGHT_BRACE");
+    var root = program.parse(tokens);
+    return root;
   }
 }
